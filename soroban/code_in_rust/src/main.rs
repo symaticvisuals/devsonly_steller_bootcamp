a14f48358358570aafbedaa4600d5dda3808b344
Deepanshu Goel <59753906+symaticvisuals@users.noreply.github.com>
2024-05-13 17:39:58 +0000

    Added Code

---
+++ soroban/code_in_rust/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+  let x: i32 = 16;
+  println("{}", x);
+
+  let z: String = String::from("Hello, Soroban!");
+  let y: &str = "Hello, Steller!";
+
+  println!("{y}")
+  println!("{z}")
+
+
+  pub fn  event(){
+    let name: String = String::from("James");
+    println("{}", name);
+  }
+            
+}
